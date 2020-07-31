bda4ebdeab7185865846779b83d457a378406ef5
Dirk Pranke <dpranke@google.com>
2020-07-30 20:23:21 -0700

    Fix a Python3-ism for python/plugin_pb2.py.
    Bug: 1112471
    Change-Id: I8f9c58fc4b76daf27567afe2cb189c06676d8468

--- python/plugin_pb2.py
+++ python/plugin_pb2.py
@@ -17,6 +17,9 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   serialized_pb='\n%google/protobuf/compiler/plugin.proto\x12\x18google.protobuf.compiler\x1a google/protobuf/descriptor.proto\"}\n\x14\x43odeGeneratorRequest\x12\x18\n\x10\x66ile_to_generate\x18\x01 \x03(\t\x12\x11\n\tparameter\x18\x02 \x01(\t\x12\x38\n\nproto_file\x18\x0f \x03(\x0b\x32$.google.protobuf.FileDescriptorProto\"\xaa\x01\n\x15\x43odeGeneratorResponse\x12\r\n\x05\x65rror\x18\x01 \x01(\t\x12\x42\n\x04\x66ile\x18\x0f \x03(\x0b\x32\x34.google.protobuf.compiler.CodeGeneratorResponse.File\x1a>\n\x04\x46ile\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x17\n\x0finsertion_point\x18\x02 \x01(\t\x12\x0f\n\x07\x63ontent\x18\x0f \x01(\tB,\n\x1c\x63om.google.protobuf.compilerB\x0cPluginProtos')
 
 
+import sys
+if sys.version_info.major >= 3:
+    unicode = lambda x, y='utf8': x
 
 
 _CODEGENERATORREQUEST = _descriptor.Descriptor(
